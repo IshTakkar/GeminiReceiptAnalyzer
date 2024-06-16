9e164e2cd3c33642a94eeb0f7c45cb39261f203b
itakkar <itakkar@asu.edu>
2024-06-16 10:44:42 -0700

    Initial Commit

---
+++ GeminiReceiptAnalyzer/GeminiReceiptAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiReceiptAnalyzerApp.swift
+//  GeminiReceiptAnalyzer
+//
+//  Created by Ish Takkar on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct GeminiReceiptAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
